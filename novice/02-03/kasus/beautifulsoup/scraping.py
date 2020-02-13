a2fb4ae4f8c357dc002810e0aff60e5eef2dec6a
Kadek Cahya <kdkchy@gmail.com>
2020-02-13 14:50:01 +0700

    init

--- novice/02-03/kasus/beautifulsoup/scraping.py
+++ novice/02-03/kasus/beautifulsoup/scraping.py
@@ -9,9 +9,12 @@ def getUrl(url):
     artist_name_list = soup.find(class_='BodyText')
     artist_name_list_items = artist_name_list.find_all('a')
 
+# Create for loop to print out all artists' names
     for artist_name in artist_name_list_items:
         print(artist_name.prettify())
 
 if __name__ == "__main__":
     import sys
     getUrl(str(sys.argv[1]))
+
+# https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm
