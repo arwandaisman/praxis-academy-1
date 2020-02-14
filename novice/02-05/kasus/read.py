ad075dc61e836b6c6bd1cc4dc934c4f7048c1f1e
Kadek Cahya <kdkchy@gmail.com>
2020-02-14 15:53:14 +0700

    init

--- novice/02-05/kasus/read.py
+++ novice/02-05/kasus/read.py
@@ -1,10 +1,21 @@
 import connect
 
 cursor = connect.db.cursor()
-sql = """select members.name,rent_mv.movie_rent from rent_mv,members where members.name="Janet Jones" AND rent_mv.member_id=1"""
+#sql = """select members.name,rent_mv.movie_rent from rent_mv,members where members.name="Janet Jones" AND rent_mv.member_id=1"""
+sql= "select member_id from members where member_id=1"
 cursor.execute(sql)
 
 results = cursor.fetchall()
 
+
 for data in results:
-  print(data)
+  a = str(data).replace('(','').replace(',','').replace(')','')
+  #print(data)
+
+print(a)
+
+sql2="select rent_mv.movie_rent from rent_mv where rent_mv.member_id={}".format(a)
+cursor.execute(sql2)
+
+result = cursor.fetchall()
+print(result)
