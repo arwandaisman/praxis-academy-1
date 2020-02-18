b25e960cbcd965d83e61cb6aa36c58bcbe28c58e
Kadek Cahya <kdkchy@gmail.com>
2020-02-18 22:16:01 +0700

    init

--- novice/03-02/latihan/hello.py
+++ novice/03-02/latihan/hello.py
@@ -1,30 +1,64 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
+from flask import request
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-from markupsafe import escape
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
-    # show the subpath after /path/
     return 'Subpath %s' % escape(subpath)
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='Dxc'))
+    url_for('static', filename='style.css')
+
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
+
 
 # lsof -i:8080
 # kill -9 pid
