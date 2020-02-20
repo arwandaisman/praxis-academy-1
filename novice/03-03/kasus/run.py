0a297c54181d19fc7e0e2872a7fa40443188fe44
Kadek Cahya <kdkchy@gmail.com>
2020-02-20 14:22:29 +0700

    init

--- novice/03-03/kasus/run.py
+++ novice/03-03/kasus/run.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from markupsafe import escape
 import mysql.connector as mariadb
 app = Flask(__name__)
 
-
-
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def home():
 
     conn = mariadb.connect(
@@ -25,22 +24,78 @@ def home():
     return render_template(
         'home.html', my_string="Welcome home!", title="Home", data=data)
 
-# @app.route("/home")
-# def home():
-#     return render_template(
-#         'home.html', my_string="Welcome home!", title="Home")
-
 @app.route("/insert")
 def insert():
     return render_template(
-        'template.html', my_string="Let's insert some data!", 
-        my_list=[0,1,2,3,4,5], title="Insert")
+        'insert.html', my_string="Let's insert some data!",
+        title="Insert")
 
-@app.route("/update")
-def update():
+@app.route("/update/<int:idmember>")
+def update(idmember):
+    conn = mariadb.connect(
+        host="localhost",
+        user="root",
+        passwd="123",
+        database='rental'
+    )
+
+    cur = conn.cursor()
+    sql = "Select * from members where member_id='{}'".format(idmember)
+    cur.execute(sql)
+    results = cur.fetchall()
+    data=[]
+    for i in results:
+        data.append(i)
+    return 'asd %d' % idmember
     return render_template(
-        'template.html', my_string="You can also update your data directly.", 
-        my_list=[0,1,2,3,4,5], title="Update")
+        'update.html', my_string="You can also update your data directly.", title=a, data=data)
+
+
+@app.route("/actupdate", methods=['GET', 'POST'])
+def actupdate():
+    if (request.method == 'POST'):
+        try:
+            name=request.form['name']
+            address=request.form['address']
+            conn = mariadb.connect(
+                host="localhost",
+                user="root",
+                passwd="123",
+                database='rental'
+            )
+
+            cur = conn.cursor()
+            sql = "UPDATE members set name='{}', address='{}' where member_id=1".format(name,address)
+            cur.execute(sql)
+            conn.commit()
+            return render_template(
+                'msg.html', my_string="Data Has Been Stored.", title="Insert")
+        except:
+            return render_template(
+                'msg.html', my_string="Databases Connection Error!", title="Insert")
+
+@app.route("/act", methods=['GET', 'POST'])
+def act():
+    if (request.method == 'POST'):
+        try:
+            name=request.form['name']
+            address=request.form['address']
+            conn = mariadb.connect(
+                host="localhost",
+                user="root",
+                passwd="123",
+                database='rental'
+            )
+
+            cur = conn.cursor()
+            sql = "INSERT INTO members(name,address) VALUES ('{}','{}')".format(name,address)
+            cur.execute(sql)
+            conn.commit()
+            return render_template(
+                'msg.html', my_string="Data Has Been Stored.", title="Insert")
+        except:
+            return render_template(
+                'msg.html', my_string="Databases Connection Error!", title="Insert")
 
 if __name__ == '__main__':
     app.run(debug=True)
